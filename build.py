d4d06f23cc8b1144df1ffef14d3019a945e547d6
Pixel-Master <102541954+Pixel-Master@users.noreply.github.com>
2024-01-29 21:48:37 +0100

    Switched to Nuitka

--- build.py
+++ build.py
@@ -6,78 +6,73 @@
 # which should be included with this package. The terms are also available at
 # http://www.gnu.org/licenses/gpl-3.0.html
 
-# This is a script used to build File Find with py2app and is not meant to be imported
+# This is a script used to build File Find with nuitka and is not meant to be imported
 
 # Imports
-from setuptools import setup
 import subprocess
 import os
 import sys
+import plistlib
+
+# Projects Libraries
+import FF_Files
 
 # Cleaning
-os.system("rm -rf dist build")
+os.system("rm -rf dist build File-Find.build File-Find.dist")
+# Creating a distribution dir
+os.mkdir(os.path.join(os.getcwd(), "dist"))
 
 # Finding and Setting architecture
 arch = subprocess.run(["uname", "-m"], capture_output=True, text=True, check=True).stdout.replace("\n", "")
-
 print("On:", arch)
-if arch != "arm64":
-    arch = "x86_64"
-sys.argv.append("py2app")
 
-APP = ["File-Find.py"]
-DATA_FILES = []
+# On macOS
+if sys.platform == "darwin":
+    # Building App
+    subprocess.run(["nuitka3",
+                    "--standalone",
+                    "--macos-create-app-bundle",
+                    f"--macos-app-icon={os.getcwd()}/assets/icon.icns",
+                    "--enable-plugin=pyside6",
+                    "File-Find.py"])
+    # Renaming and moving the app
+    subprocess.run(["mv", "File-Find.app", os.path.join("dist", "File Find.app")])
+    # Setting the plist
+    with open(os.path.join(os.getcwd(), "dist", "File Find.app", "Contents", "Info.plist"), "wb") as plist:
+        plistlib.dump(value=
+            {'CFBundleDisplayName': 'File Find',
+             "CFBundleExecutable": "File-Find",
+             "CFBundleIconFile": "icon.icns",
+             "CFBundleDocumentTypes": [{"CFBundleTypeExtensions": ["FFSearch"],
+                                        "CFBundleTypeIconSystemGenerated": True,
+                                        "CFBundleTypeName": "File Find Search",
+                                        "CFBundleTypeOSTypes": ["FFSEARCH"],
+                                        "CFBundleTypeRole": "Viewer",
+                                        "LSIsAppleDefaultForType": True}],
+             "CFBundleIdentifier": "io.github.pixel-master.file-find",
+             "CFBundleShortVersionString": FF_Files.VERSION_SHORT,
+             "CFBundleVersion": FF_Files.VERSION,
+             "CFBundleName": "File-Find",
+             "CFBundlePackageType": "APPL",
+             "LSApplicationCategoryType": "public.app-category.utilities",
+             "LSUIElement": False,
+             "NSHumanReadableCopyright": "Copyright © 2022–2024 Pixel Master. Some rights reserved.",
+             "NSSupportsSuddenTermination": False,
+             "CFBundleInfoDictionaryVersion": "6.0",
+             "NSHighResolutionCapable": True}, fp=plist)
 
-# py2app Options
-OPTIONS = {"arch": arch,
-           "iconfile": os.path.join(os.getcwd(), "assets", "icon.icns"),
-           # Plist Options
-           "plist": {"LSUIElement": False,
-                     "NSHumanReadableCopyright": "Copyright © 2022–2024 Pixel Master. All rights reserved.",
-                     "CFBundleIdentifier": "io.github.pixel-master.file-find",
-                     "CFBundleShortVersionString": "0.0",
-                     "LSApplicationCategoryType": "public.app-category.utilities",
-                     "NSSupportsSuddenTermination": True,
-                     "CFBundleDocumentTypes":
-                         [{"CFBundleTypeExtensions": ["FFSearch"],
-                           "CFBundleTypeName": "File Find Search",
-                           "CFBundleTypeOSTypes": ["FFSEARCH"],
-                           "CFBundleTypeRole": "Viewer",
-                           "LSIsAppleDefaultForType": True,
-                           "CFBundleTypeIconSystemGenerated": True}]}}
-# Building App
-setup(
-    app=APP,
-    data_files=DATA_FILES,
-    options={"py2app": OPTIONS},
-    setup_requires=["py2app"],
-    version="1.0",
-    name="File Find",
-)
+    # Building DMG
+    print("\n\nBuilding DMG...")
 
-# Building DMG
-print("\n\nBuilding DMG...")
-print('''os.system("create-dmg "
-          "--volname \"File Find\" "
-          "--volicon \"assets/icon.icns\" "
-          "--background \"assets/background.png\" "
-          "--window-pos 200 120 "
-          "--window-size 800 400 "
-          "--icon-size 100 "
-          "--icon \"dist/File Find.app\" 200 190 "
-          "--app-drop-link 600 190 "
-          "\"dist/File Find.dmg\" "
-          "\"dist/\"\n")''')
-
-subprocess.run(
-    ['create-dmg',
-     '--volname', 'File Find',
-     '--volicon', 'assets/icon.icns',
-     '--background', 'assets/background.png',
-     '--window-pos', '200', '120',
-     '--window-size', '800', '400',
-     '--icon-size', '100',
-     '--icon', 'dist/File Find.app', '200', '190',
-     '--app-drop-link', '600', '190',
-     'dist/File Find.dmg',
-     'dist'])
+    subprocess.run(
+        ['create-dmg',
+         '--volname', 'File Find',
+         '--volicon', 'assets/icon.icns',
+         '--background', 'assets/background.png',
+         '--window-pos', '200', '120',
+         '--window-size', '800', '400',
+         '--icon-size', '100',
+         '--icon', 'dist/File Find.app', '200', '190',
+         '--app-drop-link', '600', '190',
+         'dist/File Find.dmg',
+         'dist'])
